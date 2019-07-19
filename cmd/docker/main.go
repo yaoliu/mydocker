a02290ca510080838d87c921504a4bbb2c567ae6
liuyao <liuyao@ixianlai.com>
2019-07-19 16:53:52 +0800

    namespace

---
+++ cmd/docker/main.go
@@ -0,0 +1,27 @@
+//__author__ = "YaoYao"
+//Date: 2019-07-19
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
+		//Credential:&syscall.Credential{Uid: uint32(1), Gid: uint32(1)},
+	}
+	//cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(-1)
+}
